eb9d2c03d7a21605263dffc326171aad39a7e2bc
asadzaman2012 <asad.zaman@patch.com>
2013-06-19 15:32:31 -0400

    test

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,30 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class DemoTest 
+{
+	WebDriver driver = new FirefoxDriver();
+	
+	@Before
+	public void before()
+	{
+		driver.get("http://bellmore.nixon.i.patch.com");
+	}
+	
+	@Test
+	public void test1()
+	{
+		
+	}
+	
+	@After
+	public void close()
+	{
+		driver.quit();
+	}
+
+}
